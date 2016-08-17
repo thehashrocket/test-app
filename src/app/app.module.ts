6c2309179d17c58a05caaa8e57220fc2619ba72f
YOUR NAME <jasshultz@gmail.com>
2016-08-16 20:50:36 -0600

    init commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,14 @@ import { NgModule, ApplicationRef } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
+import { AngularFireModule } from 'angularfire2';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
+    AngularFireModule.initializeApp(firebaseConfig, firebaseAuthConfig),
     BrowserModule,
     CommonModule,
     FormsModule
